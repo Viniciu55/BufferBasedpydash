1fc10b4ff2a7d34e93401dc7ca611f80c6a5a27f
Vinicius <sd.vinicius.main@gmail.com>
2024-09-07 13:06:58 -0300

    bufferbasedV1

--- r2a/r2abufferbased.py
+++ r2a/r2abufferbased.py
@@ -26,10 +26,10 @@ class R2ABufferBased(IR2A):
 
     def handle_segment_size_request(self, msg):
         # definir a qualidade
-        print("===========================================")
+        print("==========================================")
         b = self.whiteboard.get_playback_buffer_size()
         Qselec = 0
-        bufferlists = range(0,100,5) #seleciona a lista de qualidades 
+        bufferlist = range(0,100,5) #lista de qualidades possíveis
 
         #verifica se existem informações de buffer, caso contrário, o buffer é =5
         if len(b)>0:
@@ -38,46 +38,47 @@ class R2ABufferBased(IR2A):
         else:
             buffer = 5
 
+        #verifica o tamanho do buffer atual e seta a qualidade a partir disso
         if buffer>0:
-            if buffer < bufferlists[0]:
+            if buffer < bufferlist[0]:
                 Qselec = 0
-            elif buffer < bufferlists[1]:
+            elif buffer < bufferlist[1]:
                 Qselec = 1
-            elif buffer < bufferlists[2]:
+            elif buffer < bufferlist[2]:
                 Qselec = 2
-            elif buffer <= bufferlists[3]:
+            elif buffer <= bufferlist[3]:
                 Qselec = 3
-            elif buffer < bufferlists[4]:
+            elif buffer < bufferlist[4]:
                 Qselec = 4
-            elif buffer < bufferlists[5]:
+            elif buffer < bufferlist[5]:
                 Qselec = 5
-            elif buffer < bufferlists[6]:
+            elif buffer < bufferlist[6]:
                 Qselec = 6
-            elif buffer < bufferlists[7]:
+            elif buffer < bufferlist[7]:
                 Qselec = 7
-            elif buffer < bufferlists[8]:
+            elif buffer < bufferlist[8]:
                 Qselec = 8
-            elif buffer < bufferlists[9]:
+            elif buffer < bufferlist[9]:
                 Qselec = 9
-            elif buffer < bufferlists[10]:
+            elif buffer < bufferlist[10]:
                 Qselec = 10
-            elif buffer < bufferlists[11]:
+            elif buffer < bufferlist[11]:
                 Qselec = 11
-            elif buffer < bufferlists[12]:
+            elif buffer < bufferlist[12]:
                 Qselec = 12
-            elif buffer < bufferlists[13]:
+            elif buffer < bufferlist[13]:
                 Qselec = 13
-            elif buffer < bufferlists[14]:
+            elif buffer < bufferlist[14]:
                 Qselec = 14
-            elif buffer < bufferlists[15]:
+            elif buffer < bufferlist[15]:
                 Qselec = 15
-            elif buffer < bufferlists[16]:
+            elif buffer < bufferlist[16]:
                 Qselec = 16
-            elif buffer < bufferlists[17]:
+            elif buffer < bufferlist[17]:
                 Qselec = 17
-            elif buffer < bufferlists[18]:
+            elif buffer < bufferlist[18]:
                 Qselec = 18
-            elif buffer < bufferlists[19]:
+            elif buffer < bufferlist[19]:
                 Qselec = 19
             
 
